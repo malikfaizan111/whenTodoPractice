9ef6c09b30348e6d428e2ccbec0ad2b7cf9f6b1b
malikfaizan111 <faizanatifmasood@gmail.com>
2021-11-20 02:47:09 +0500

    push changes
    resposiveness and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,37 @@ import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
 import { TemplateComponent } from './template/template.component';
-import { SignupComponent } from './signup/signup.component';
-import { SampletempComponent } from './sampletemp/sampletemp.component';
+import { SampletempComponent } from './views/sampletemp/sampletemp.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { EmployeesComponent } from './views/employees/employees.component';
+import { TeamsComponent } from './views/teams/teams.component';
+import { DesignationComponent } from './views/designation/designation.component';
+import { SupervisorsComponent } from './views/supervisors/supervisors.component';
+import { QueriesComponent } from './views/queries/queries.component';
+import { MyqueriesComponent } from './views/myqueries/myqueries.component';
+import { QAqueriesComponent } from './views/qaqueries/qaqueries.component';
+import { ReportsComponent } from './views/reports/reports.component';
+import { RegisterComponent } from './views/register/register.component';
+import { LogoutComponent } from './views/logout/logout.component';
+import { DepartmentsComponent } from './views/departments/departments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
-    SignupComponent,
-    SampletempComponent
+    SampletempComponent,
+    DashboardComponent,
+    EmployeesComponent,
+    TeamsComponent,
+    DesignationComponent,
+    SupervisorsComponent,
+    QueriesComponent,
+    MyqueriesComponent,
+    QAqueriesComponent,
+    ReportsComponent,
+    RegisterComponent,
+    LogoutComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule,
